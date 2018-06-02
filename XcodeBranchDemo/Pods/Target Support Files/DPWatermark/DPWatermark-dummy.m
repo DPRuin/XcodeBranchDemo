05583a9350be6c793c6252a23283a5d617654552
mac126 <tulaomao152@126.com>
2018-06-02 18:37:45 +0800

    添加自己的自定义库DPWatermark

---
+++ XcodeBranchDemo/Pods/Target Support Files/DPWatermark/DPWatermark-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DPWatermark : NSObject
+@end
+@implementation PodsDummy_DPWatermark
+@end
---
+++ XcodeBranchDemo/Pods/Target Support Files/Pods-ardemo/Pods-ardemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ardemo : NSObject
+@end
+@implementation PodsDummy_Pods_ardemo
+@end
