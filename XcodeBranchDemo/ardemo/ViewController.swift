beafbf851aaba99eb1fcb038d61bbb29ec037911
mac126 <tulaomao152@126.com>
2018-06-30 10:59:57 +0800

    xiaofanglizhile

--- XcodeBranchDemo/ardemo/ViewController.swift
+++ XcodeBranchDemo/ardemo/ViewController.swift
@@ -40,7 +40,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // 课余时间做个AR游戏吧
         makeARDragonGame()
         whaitIsgugeAnimation()
+        xiaofanglizhile()
         
+    }
+    
+    func xiaofanglizhile() {
+        print("xiaofanglizhile离职了")
     }
     
     func whaitIsgugeAnimation() {
